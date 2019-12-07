7462e1aa882d5c4e1527de50ddb4ef62a72b6369
viri <git@stuff.moe>
2019-12-07 01:19:27 -0600

    add the one & only get function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+//! Queries the major, minor and build version of Windows (NT) efficiently
+//! with usage of undocumented ntdll functions.
+//!
+//! It only has one function: [get](fn.get.html).
+
+#[link(name = "ntdll")]
+extern "C" {
+    fn RtlGetNtVersionNumbers(major: *mut u32, minor: *mut u32, build: *mut u32);
+}
+
+/// Queries the (major, minor, build) version of the Windows NT system.
+pub fn get() -> (u32, u32, u32) {
+    let (mut major, mut minor, mut build) = (0u32, 0u32, 0u32);
+    unsafe {
+        RtlGetNtVersionNumbers(&mut major as _, &mut minor as _, &mut build as _);
     }
+    (major, minor, build)
 }
