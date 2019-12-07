eeccf8800346525e79fdb025655a92b3b59c0aec
viri <git@stuff.moe>
2019-12-07 02:39:11 -0600

    cargo, lib, readme: add fallback feature
    ... and also improve the readme with a `why?` section.

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # nt_version
-Queries the major, minor and build version of Windows (NT) efficiently with usage of undocumented ntdll functions.
+Queries the major, minor, and build version of Windows (NT) efficiently with usage of undocumented NTDLL functions.
+**Needs a minimum version of NT 5.1! (Windows XP or above)**
+```toml
+nt_version = "0.1"
+```
+If building fails with a linker error, you're missing `ntdll.lib` from your system.
+It doesn't come on older versions of Windows with the SDK and you need to install the DDK.
 
+Alternatively, enable the fallback feature which queries the function pointer at runtime (but is slower):
+```toml
+nt_version = { version = "0.1", features = ["fallback"] }
+```
 
 ## Usage
-It only has one function: [get](https://www.google.com/), and it's recommended you use it explicitly.
+It only has one function: [get](https://www.google.com/), and it's recommended you use it explicitly:
 
 ```rust
 fn main() {
@@ -11,3 +21,7 @@ fn main() {
     println!("NT Version v{}.{}.{}", major, minor, build);
 }
 ```
+
+## Why?
+Microsoft deprecated [GetVersionEx](https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getversionexa)
+in Windows 8.1 and it's very difficult to get reliable exact OS versions now.
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,46 @@
 //! Queries the major, minor and build version of Windows (NT) efficiently
-//! with usage of undocumented ntdll functions.
+//! with usage of undocumented NTDLL functions.
 //!
 //! It only has one function: [get](fn.get.html),
 //! and it's recommended you use it explicitly like `nt_version::get()` because of this.
 
-#[link(name = "ntdll")]
-extern "C" {
-    fn RtlGetNtVersionNumbers(major: *mut u32, minor: *mut u32, build: *mut u32);
+#[cfg(not(target_os = "windows"))]
+compile_error!("This crate is for querying Windows, but the target isn't Windows.");
+
+#[cfg(not(feature = "fallback"))]
+mod internal {
+    #[link(name = "ntdll")]
+    extern "C" {
+        pub fn RtlGetNtVersionNumbers(major: *mut u32, minor: *mut u32, build: *mut u32);
+    }
+}
+
+#[cfg(feature = "fallback")]
+mod internal {
+    #![allow(non_snake_case)]
+
+    use std::{mem::transmute, ptr};
+    use winapi::um::libloaderapi::{GetModuleHandleA, GetProcAddress};
+
+    static mut NTDLL_FUNCTION: *const () = ptr::null();
+    pub unsafe fn RtlGetNtVersionNumbers(major: *mut u32, minor: *mut u32, build: *mut u32) {
+        if NTDLL_FUNCTION.is_null() {
+            NTDLL_FUNCTION = GetProcAddress(
+                GetModuleHandleA(b"ntdll.dll\0".as_ptr() as *const _),
+                b"RtlGetNtVersionNumbers\0".as_ptr() as *const _,
+            ) as *const _;
+        }
+        transmute::<_, extern "stdcall" fn(*mut u32, *mut u32, *mut u32)>(NTDLL_FUNCTION)(
+            major, minor, build,
+        );
+    }
 }
 
 /// Queries the (major, minor, build) version of the Windows NT system.
 pub fn get() -> (u32, u32, u32) {
     let (mut major, mut minor, mut build) = (0u32, 0u32, 0u32);
     unsafe {
-        RtlGetNtVersionNumbers(&mut major as _, &mut minor as _, &mut build as _);
+        internal::RtlGetNtVersionNumbers(&mut major as _, &mut minor as _, &mut build as _);
     }
     (major, minor, build)
 }
